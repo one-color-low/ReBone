444e27e0b6ceb3ee54ef9a0186b0928259173c95
itagaki <i1931011@uec.ac.jp>
2019-10-22 23:57:17 +0900

    いっぱい

--- app.py
+++ app.py
@@ -4,55 +4,104 @@ from flask_sqlalchemy import SQLAlchemy
  
 app = Flask(__name__)
 
+# ツール
+
+def get_path(type, name):
+    if type == "model":
+        # pmd or pmxで場合分けする必要がある
+        if name == "miku_1":
+            return 'static/models/'+name+'/'+name+'.pmd'
+        else:
+            return 'static/models/'+name+'/'+name+'.pmx'
+    elif type == "background":
+        return 'static/backgrounds/'+name+'.jpg'
+    elif type == "sound":
+        return 'static/sounds/'+name+'.mp3'
+    elif type == "vmd":
+        return 'static/vmds/'+name+'.vmd'
+    elif type == "subtitle":
+        return 'static/subtitles/'+name+'.json'
+
+def is_valid(room_name):    
+    table = Entry.query.all()
+    for row in table:
+        if row.room_name == room_name:
+            return False
+    return True
+
+def is_exist(room_name):
+    table = Entry.query.all()
+    for row in table:
+        if row.room_name == room_name:
+            return True
+    return False
+
 # ルーティング
-@app.route('/createroom', methods=['POST', 'GET'])
+@app.route('/', methods=['GET','POST'])
 def createroom():
     if request.method == 'POST':
-        if(valid_rooom_name(request.form["room_name"])):
-            return render_template('recording.html', model=request.form["model"], background=request.form["background"], room_name=request.form["room_name"])
+        if(is_valid(request.form["room_name"])):
+            add_entry(
+                room_name = request.form["room_name"], 
+                model_path = get_path('model', request.form["model"]),
+                background_path = get_path('background', request.form["background"]),
+                sound_path = "", vmd_path = "", subtitle_path = "") 
+            next_url = "http://localhost:5000/Vstudio?room_name="+request.form["room_name"]
+            return render_template('show_link_and_QRcode.html', url=next_url)
         else:
-            return render_template('createroom.html', message="このルーム名は既に使われています。")
-    return render_template('createroom.html', message = "")
+            return render_template('createroom.html', pre_val=request.form, message="このルーム名は既に使われています。")
+    else:
+        return render_template('createroom.html', pre_val=[], message = "")
 
-    def valid_rooom_name(room_name):    
-        # todo: DBを参照してルーム名の重複を検知
-        return True
 
+@app.route('/Vstudio', methods=['POST', 'GET'])
+def Vstudio():
+    if is_exist(request.args.get('room_name','')):
+        if request.method == 'POST':
+            update_entry(
+                room_name = request.args.get('room_name',''), 
+                model_path = None,
+                background_path = None,
+                sound_path = get_path('sound', request.form['sound']), #todo: postで受け取った値に対応
+                vmd_path = get_path('vmd', request.form['vmd']), #todo: postで受け取った値に対応
+                subtitle_path = get_path('subtitle', 'sample')) 
+            next_url = "http://localhost:5000/Vroom?room_name="+request.args.get('room_name')
+            return render_template('show_link_and_QRcode.html', url=next_url)
+        else: 
+            return render_template('Vstudio.html')
+    else: 
+        return render_template('not_found.html', message="ルームが指定されていないか、指定のルームが見つかりません。")
+    
 
 @app.route('/Vroom', methods=['POST', 'GET'])
 def Vroom():
-    if request.method == 'GET':
-        access_target_room(request.args.get)
+    if is_exist(request.args.get('room_name')):
+        entry = Entry.query.filter(Entry.room_name == request.args.get('room_name') ).first()
+        return render_template('Vroom.html', 
+            model_path = entry.model_path,
+            background_path = entry.background_path,
+            sound_path = entry.sound_path,
+            vmd_path = entry.vmd_path,
+            subtitle_path = entry.subtitle_path )
     else:
-        access_room_list()
-
-    def access_target_room(room):
-        # todo: DBからroomの各種データを取得
-        model_path="/"
-        background_path="/"
-        vmd="sss"   # blobでデータベースに直接保存 or EC2のパス
-        return render_template('Vroom.html',room_name=room, model_path=model_path, background_path=background_path, vmd=vmd )
-
-    def access_room_list():
-        # todo: DBから登録済みルーム一覧を取得
-        room_list = ["aaa","bbb","ccc"]
-        return render_template("Vroom_list.html", room_list=room_list)
+        room_list = get_list_from_db()
+        return render_template('list.html', room_list=room_list)
 
 
 @app.route('/runanime')
 def runanime():
-    return render_template('index.html') 
+    return render_template('runanime.html') 
  
 
 # データベース
-db_uri = os.environ.get('DATABASE_URL') or "postgresql://localhost/flaskvtube"
+#db_uri = os.environ.get('DATABASE_URL') or "postgresql://localhost/flaskvtube"
+db_uri = "sqlite:///" + os.path.join(app.root_path, 'flaskvtube.db') # 追加
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app) 
 
 class Entry(db.Model): 
-    __tablename__ = "rooms" 
-    room_id = db.Column(db.Integer, primary_key=True) 
-    room_name = db.Column(db.String(), nullable=False) 
+    __tablename__ = "rooms2" 
+    room_name = db.Column(db.String(), primary_key=True) 
     model_path = db.Column(db.String(), nullable=False) 
     background_path = db.Column(db.String(), nullable=False) 
     sound_path = db.Column(db.String(), nullable=False) 
@@ -60,22 +109,38 @@ class Entry(db.Model):
     subtitle_path = db.Column(db.String(), nullable=False) 
 
 
-@app.route('/')
-def add_entry():
+def add_entry(room_name, model_path, background_path, sound_path, vmd_path, subtitle_path):
     entry = Entry()
-    entry.room_id = 1
-    entry.room_name = "oppai"
-    entry.model_path = "path/to/model/"
+    entry.room_name = room_name
+    entry.model_path = model_path
+    entry.background_path = background_path
+    entry.sound_path = sound_path
+    entry.vmd_path = vmd_path
+    entry.subtitle_path = subtitle_path
 
     db.session.add(entry)
     db.session.commit()
-    return render_template('sql_view.html', entry=entry)
+    return 0
 
-def hello_world():
-    entries = Entry.query.all() 
-    return render_template('sql_view.html', entry=entries)
+def update_entry(room_name, model_path, background_path, sound_path, vmd_path, subtitle_path):
+    entry = Entry().query.filter(Entry.room_name == room_name).first()
+    if model_path != None:
+        entry.model_path = model_path
+    if background_path != None:
+        entry.background_path = background_path
+    if sound_path != None:
+        entry.sound_path = sound_path
+    if vmd_path != None:
+        entry.vmd_path = vmd_path
+    if subtitle_path != None:
+        entry.subtitle_path = subtitle_path
 
-'''
+    db.session.add(entry)
+    db.session.commit()
+    return 0
+
 # 実行
 if __name__ == "__main__":
-    app.run(debug=True)'''
+    app.run(debug=True)
+
+# ※entry=一連の処理
--- readme.md
+++ readme.md
@@ -13,3 +13,29 @@ create table room_info(room_id int, room_name varchar(10), model_path varchar(20
 ```
 insert into room_info values (1, 'sample_room', 'path/to/model/', 'path/to/bg/', 'path/to/sound/', 'path/to/vmd', 'path/to/subtitle');
 ```
+
+# System
+## RoomCreate
+- room_nameなどを入力
+- room_nameが使用可能か判定
+- DBに保存
+- Vstudio(?room_name=hogehoge)に遷移
+- (できれば)ajaxでモデルをリアルタイム表示
+- (できれば)編集パスワードもDBに保存
+
+## Vstudio(?room_name)
+- 録画開始ボタンを表示
+- jsで録画＆録音
+- 録画中はタイマーを表示
+- 終わったらPythonで処理
+- それか直接vmdを貼り付け
+- vmdを得たらDBに保存
+
+## Vroom(?room_name)
+- DBからroom_nameに対応する各種データを取り出し
+- それを使い, Three.jsでroomを表示
+
+# todo
+- app.py内のurlを動的に
+- カメラ＆マイク
+- qrコード
